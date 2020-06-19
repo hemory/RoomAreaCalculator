51bbbbe3bab3280ffd5bf4deeb2d56351b41bdfc
Hemory Phifer <hemphife@gmail.com>
2020-06-19 15:30:48 -0400

    first commit

---
+++ RoomAreaCalculatorApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAreaCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room roomObj = new Room();
+            Console.WriteLine("How many rooms would you like to calculate?");
+            int numberOfRooms = Convert.ToInt16(Console.ReadLine());
+
+            List<Room> rooms = new List<Room>();
+
+            for (int i = 0; i < numberOfRooms; i++)
+            {
+
+                Console.WriteLine($"Let's get the length of Room Number {i + 1}?");
+                Console.WriteLine("How many feet?");
+
+                roomObj.Length = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Let's get the width of Room Number {i + 1}?");
+                roomObj.Width = Convert.ToDouble(Console.ReadLine());
+
+                rooms.Add(roomObj);
+
+            }
+
+            double totalRoomArea = roomObj.CalculateAreaOfAllRooms(rooms);
+
+            Console.WriteLine($"The total area of all these rooms is {totalRoomArea}");
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ RoomAreaCalculatorApp/Room.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAreaCalculatorApp
+{
+   public class Room
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+
+        public double CalculateArea(double length, double width)
+        {
+            return width * length;
+        }
+
+        public double CalculateAreaOfAllRooms(List<Room> rooms)
+        {
+
+            double sum = 0;
+
+            foreach (var room in rooms)
+            {
+                double area = CalculateArea(room.Length, room.Width);
+
+                sum += area;
+
+            }
+
+            return sum;
+
+        }
+    }
+
+  
+}
